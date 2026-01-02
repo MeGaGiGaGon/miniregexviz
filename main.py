786610e06120ad6c8c19d016217f323b04087f03
MeGaGiGaGon <107241144+MeGaGiGaGon@users.noreply.github.com>
2026-01-02 14:59:56 -0800

    Trying a method-based parser approach

--- main.py
+++ main.py
@@ -1,110 +1,86 @@
 """
-Miniminiregex:
-literals: `a-zA-Z0-9`
-repeats: `+` only
-groups: `()` numbered only
+class Literal = ...
+class Repeat = (Group | Literal) _'+'
+class Group = _'(' Alt _')'
+class Alt = Concat.seperated_by('|')
+class Concat = Regex*
+class Regex = Repeat | Literal
 """
 
-"""
-literal = a-zA-Z0-9
-
-"""
-
-from collections.abc import Sequence
 from dataclasses import dataclass
-import string
 from typing import Self
 
 @dataclass
-class Data:
-    inner: str
-    current_index: int
-    span_stack: list[Span]
-
-    def push_span(self):
-        self.span_stack.append(Span(self.current_index, self.current_index))
-
-    def pop_span(self) -> Span:
-        return self.span_stack.pop()
-
-    def increment(self, by: int = 1):
-        self.current_index += by
-        self.span_stack[-1].end += self.current_index
-
-    def next_if_in(self, to_check: str) -> str | None:
-        if self.inner[self.current_index] in to_check:
-            self.increment()
-            return self.inner[self.current_index - 1]
-
-@dataclass
-class Span:
+class Error:
+    regex: None
     start: int
     end: int
-
-@dataclass
-class Error:
-    data_to_render: Regex | None
     message: str
-    span: Span
 
 @dataclass
 class Literal:
     char: str
-    span: Span
+    start: int
+    end: int
 
     @classmethod
-    def parse(cls, data: Data) -> tuple[Self, int] | None:
-        data.push_span()
-        if char := data.next_if_in(string.ascii_letters + string.digits):
-            return cls(char, data.pop_span()), data.current_index
+    def parse(cls, input: str, index: int) -> tuple[Self, int] | None:
+        if index < len(input):
+            return cls(input[index], index, index + 1), index + 1
         else:
-            _ = data.pop_span()
             return None
 
 @dataclass
 class Repeat:
-    repeated: Regex
-    span: Span
+    repeated: Literal | Group
+    start: int
+    end: int
 
     @classmethod
     def parse(cls, input: str, index: int) -> tuple[Self | Error, int] | None:
-        match Regex.parse(input, index):
-            case None:
-                if input[index] == "+":
-                    return Error(None, "Repeat with nothing to repeat", Span(index, index + 1)), index + 1
-                else:
-                    return None
-            case (regex, new_index):
-                if input[index] == "+":
-                    return cls(regex, Span(index, new_index + 1)), new_index + 1
-                else:
-                    return None
-
-@dataclass
-class Group:
-    contents: Concat
-    span: Span
-
-    @classmethod
-    def parse(cls, input: str, index: int) -> tuple[Self | Error, int] | None:
-        if input[index] == "(":
-            contents, new_index = Concat.parse(input, index + 1)
-            if input[new_index] == ")":
-                return cls()
+        result = Group.parse(input, index)
+        if result is None:
+            result = Literal.parse(input, index)
+            if result is None:
+                return Error(None, index, index + 1, "Repeat with nothing to repeat"), index + 1
+        repeated, new_index = result
+        if input[new_index] == "+":
+            return cls(repeated, index, new_index + 1), new_index + 1
         else:
             return None
 
 @dataclass
-class Concat:
-    regexes: Sequence[Regex]
-    span: Span
+class Group:
+    grouped: Alt
+    start: int
+    end: int
 
     @classmethod
-    def parse(cls, input: str, index: int) -> tuple[Self, int]:
+    def parse(cls, input: str, index: int) -> tuple[Self, int] | None:
+        raise NotImplementedError
+
+@dataclass
+class Alt:
+    start: int
+    end: int
+
+    @classmethod
+    def parse(cls, input: str, index: int) -> tuple[Self, int] | None:
+        raise NotImplementedError
+
+@dataclass
+class Concat:
+    start: int
+    end: int
+
+    @classmethod
+    def parse(cls, input: str, index: int) -> tuple[Self, int] | None:
         raise NotImplementedError
 
 @dataclass
 class Regex:
+    start: int
+    end: int
 
     @classmethod
     def parse(cls, input: str, index: int) -> tuple[Self, int] | None:
