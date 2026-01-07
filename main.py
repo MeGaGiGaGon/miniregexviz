9256cab570ddd21e32e432ea65c60ddf2c2aaf69
GiGaGon <107241144+MeGaGiGaGon@users.noreply.github.com>
2026-01-06 23:38:48 -0800

    Add basic CLI

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
+"""
+Usage:
+main.py: run gui
+main.py parse "regex": print parsed result of "regex"
+main.py match "regex" "data" start?: print where the index regex matches data till, starting at start
+main.py scan "regex" "data" start?: print where the index regex matches data till, scanning from start
+
+Add --pdb to drop into debugger on run
+"""
+
+import sys
 from src.gui import Editor
+from src.lexer_parser import parse
+from src.matcher import matches, scan
 
 
 def main():
@@ -6,4 +19,23 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    args = sys.argv
+    if args and args[0].endswith("main.py"):
+        args = args[1:]
+
+    if "--pdb" in args:
+        args.remove("--pdb")
+        breakpoint()
+
+    match args:
+        case []:
+            main()
+        case ["parse", regex_source]:
+            for index, regex in enumerate(parse(regex_source)):
+                print(index, regex)
+        case ["match", regex_source, target, *rest] if (len(rest) == 1 and (start:=int(rest[0]))) or len(rest) == (start:=0):
+            print(matches(parse(regex_source), target, start))
+        case ["scan", regex_source, target, *rest] if (len(rest) == 1 and (start:=int(rest[0]))) or len(rest) == (start:=0):
+            print(scan(parse(regex_source), target, start))
+        case _:
+            print(__doc__)
