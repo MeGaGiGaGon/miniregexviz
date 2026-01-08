f1f0bbc600072d1e600d301d5f8030f169eabf1f
MeGaGiGaGon <107241144+MeGaGiGaGon@users.noreply.github.com>
2026-01-08 12:30:11 -0800

    Remove some spaghetti from parser

--- src/lexer_parser.py
+++ src/lexer_parser.py
@@ -5,7 +5,7 @@ Currently just a parser since the lexer is not needed yet.
 """
 
 
-from typing import TYPE_CHECKING, TypeGuard
+from typing import TYPE_CHECKING, NamedTuple, TypeGuard
 
 from src.regex_ast import (
     AltEnd,
@@ -23,52 +23,66 @@ if TYPE_CHECKING:
 def is_regex_sequence(data: Sequence[object]) -> TypeGuard[Sequence[Regex]]:
     return all(isinstance(x, Regex.__value__) for x in data)  # pyright: ignore[reportAny]
 
+class InProgressGroup(NamedTuple):
+    group_start_index: int
+    group_index: int
+    alts: list[InProgressAlt]
+
+class InProgressAlt(NamedTuple):
+    start_regex_index: int
+    concat: list[Regex]
+
 def parse(source: str) -> Sequence[Regex]:
     if not source:
         return [GroupStart(0, 0, "", 0, [1]), GroupEnd(0, 0, "", 0, 0)]
 
-    group_stack: list[tuple[int, int, list[tuple[int, list[Regex]]]]] = [(-1, 0, [(1, [])])]
+    group_stack: list[InProgressGroup] = [InProgressGroup(-1, 0, [InProgressAlt(1, [])])]
+
+    def last_concat() -> list[Regex]:
+        return group_stack[-1].alts[-1].concat
 
     def fold_last_group(finished: bool) -> None:
         group_start_index, group_index, alts = group_stack.pop()
+        concat = last_concat()
         if finished:
-            group_stack[-1][2][-1][1].append(GroupStart(group_start_index, source_index + 1, source, group_index, [alt_start for alt_start, _ in alts[1:]]))
+            concat.append(GroupStart(group_start_index, source_index + 1, source, group_index, [alt_start for alt_start, _ in alts[1:]]))
         else:
-            group_stack[-1][2][-1][1].append(RegexError(group_start_index, group_start_index + 1, source, "Unclosed Group"))
-        group_stack[-1][2][-1][1].extend(alts[0][1])
+            concat.append(RegexError(group_start_index, group_start_index + 1, source, "Unclosed Group"))
+        concat.extend(alts[0].concat)
         for alt_start, alt in alts[1:]:
-            group_stack[-1][2][-1][1].append(AltEnd(alt_start - 1, alt_start, source, regex_index))
-            group_stack[-1][2][-1][1].extend(alt)
+            concat.append(AltEnd(alt_start - 1, alt_start, source, regex_index))
+            concat.extend(alt)
         if finished:
-            group_stack[-1][2][-1][1].append(GroupEnd(source_index, source_index + 1, source, group_start_index, group_index))
+            concat.append(GroupEnd(source_index, source_index + 1, source, group_start_index, group_index))
 
     for source_index, char in enumerate(source):
         regex_index = source_index + 1
         if char == "(":
-            group_stack.append((source_index, group_stack[-1][1] + 1, [(regex_index + 1, [])]))
+            group_stack.append(InProgressGroup(source_index, group_stack[-1].group_index + 1, [InProgressAlt(regex_index + 1, [])]))
         elif char == "|":
-            group_stack[-1][2].append((regex_index + 1, []))
+            group_stack[-1].alts.append(InProgressAlt(regex_index + 1, []))
         elif char == ")":
             if len(group_stack) == 1:
-                group_stack[-1][2][-1][1].append(RegexError(source_index, source_index + 1, source, "Unopened group"))
+                last_concat().append(RegexError(source_index, source_index + 1, source, "Unopened group"))
             else:
                 fold_last_group(True)
         elif char == "+":
-            match group_stack[-1][2][-1][1]:
+            concat = last_concat()
+            match concat:
                 case [*_, RegexLiteral(start)]:
-                    group_stack[-1][2][-1][1].append(RepeatEnd(start, source_index + 1, source, regex_index - 1))
+                    concat.append(RepeatEnd(start, source_index + 1, source, regex_index - 1))
                 case [*_, GroupEnd(start, group_start=group_start)]:
-                    group_stack[-1][2][-1][1].append(RepeatEnd(start, source_index + 1, source, group_start + 1))
+                    concat.append(RepeatEnd(start, source_index + 1, source, group_start + 1))
                 case _:
-                    group_stack[-1][2][-1][1].append(RegexError(source_index, source_index + 1, source, "Unrepeatable item"))
+                    concat.append(RegexError(source_index, source_index + 1, source, "Unrepeatable item"))
         else:
-            group_stack[-1][2][-1][1].append(RegexLiteral(source_index, source_index + 1, source, char))
+            last_concat().append(RegexLiteral(source_index, source_index + 1, source, char))
     while len(group_stack) > 1:
         fold_last_group(False)
-    group_stack.insert(0, (0, 0, [(1, [])]))
+    group_stack.insert(0, InProgressGroup(0, 0, [InProgressAlt(1, [])]))
     # Silly hack to make regexes like "a|" have the final AltEnd point at the final GroupEnd instead of themselves
     regex_index += 1  # pyright: ignore[reportPossiblyUnboundVariable]
     # Silly hack to make the final GroupEnd highlighting not appear on the last char
     source_index += 1  # pyright: ignore[reportPossiblyUnboundVariable]
     fold_last_group(True)
-    return group_stack[-1][2][-1][1]
+    return last_concat()
