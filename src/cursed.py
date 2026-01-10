4c43cc003430a88510e5a3dddcbb0a9ac9229283
MeGaGiGaGon <107241144+MeGaGiGaGon@users.noreply.github.com>
2026-01-09 16:33:22 -0800

    Update comment

--- src/cursed.py
+++ src/cursed.py
@@ -8,7 +8,7 @@ from collections.abc import Callable  # noqa: TC003
 
 
 def get_lru_cache_inputs(func: Callable[..., object]) -> list[tuple[object, ...]] | None:
-    # The lru cache __dict__ doesn't actually have a corresponding parameter, so we have to use gc to find it
+    # The lru cache's cache dict doesn't actually have a corresponding parameter, so we have to use gc to find it
     for referent in gc.get_referents(func):  # pyright: ignore[reportAny]
         # We know we've found it if it has a value of type functools._lru_cache_wrapper
         if isinstance(referent, dict) and isinstance(next(iter(referent.values()), None), functools._lru_cache_wrapper):  # pyright: ignore[reportPrivateUsage, reportUnknownArgumentType]  # noqa: SLF001
