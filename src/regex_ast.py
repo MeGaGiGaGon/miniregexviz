e509fcae485c3dc260fad7eeaff4fe1610247a50
MeGaGiGaGon <107241144+MeGaGiGaGon@users.noreply.github.com>
2026-01-09 18:21:27 -0800

    Update AST to support full python syntax

--- src/regex_ast.py
+++ src/regex_ast.py
@@ -4,7 +4,7 @@ Types representing the regex AST.
 
 
 from dataclasses import dataclass
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Literal, NamedTuple
 
 if TYPE_CHECKING:
     from collections.abc import Sequence
@@ -23,12 +23,34 @@ class Spanned:
     def length(self) -> int:
         return self.end - self.start
 
+class SingleChar(NamedTuple):
+    char: str
+    escaped_special: bool
+
+class CharSet(NamedTuple):
+    negative: bool
+    chars: str  # TODO: This will make performance sad, use something better in the future
+
+class Backref(NamedTuple):
+    type: Literal["Name", "Number"]
+    group_index: int
+
+class ShortCharSet(NamedTuple):
+    type: Literal["d", "D", "s", "S", "w", "W"]
+
+
 @dataclass(frozen=True)
 class RegexLiteral(Spanned):
     """
-    A singular literal character.
+    A char/set of chars/something like that for matching against the source string.
     """
-    char: str
+
+    literal: SingleChar | CharSet | Backref | ShortCharSet
+
+@dataclass(frozen=True)
+class ZeroWidthRegexLiteral(Spanned):
+    "Seperate from RegexLiteral because of repeatability checking"
+    char: Literal["A", "b", "B", "z", "Z", "^", "$"]
 
 @dataclass(frozen=True)
 class AltEnd(Spanned):
@@ -37,13 +59,35 @@ class AltEnd(Spanned):
     """
     jump_to: int
 
+class Capturing(NamedTuple):
+    type: Literal["Numbered", "Named"]
+    group_index: int
+    group_name: str| None
+
+class Noncapturing(NamedTuple):
+    type: Literal["Noncapturing", "Atomic"]
+
+class InlineFlags(NamedTuple):
+    inline_flags: tuple[set[Literal["aiLmsux"]], set[Literal["imsx"]]]
+
+class Lookaround(NamedTuple):
+    type: Literal["Lookahead", "Lookbehind"]
+    negated: bool
+
+
 @dataclass(frozen=True)
 class GroupStart(Spanned):
     """
     The start of a group, used for tracking concats and where repeats should point.
     """
-    group_index: int
+    type: Capturing | Noncapturing | InlineFlags | Lookaround
     concat_indexes: Sequence[int]
+
+@dataclass(frozen=True)
+class Conditional(Spanned):
+    group_index: int
+    yes: int
+    no: int | None
 
 @dataclass(frozen=True)
 class GroupEnd(Spanned):
@@ -54,11 +98,22 @@ class GroupEnd(Spanned):
     group_index: int
 
 @dataclass(frozen=True)
+class RepeatStart(Spanned):
+    """
+    A Marker used for repeats that accept 0 repetitions.
+    """
+    repeat_end: int
+    mode: Literal["greedy", "lazy", "possessive"]
+
+@dataclass(frozen=True)
 class RepeatEnd(Spanned):
     """
-    The end of a greedy repeat, matches if the repeated matched at least once.
+    The end of a repeat, matches if the repeated matches between minimum and maxium times.
     """
     repeat_start: int
+    mode: Literal["greedy", "lazy", "possessive"]
+    minimum: int
+    maximum: int
 
 @dataclass(frozen=True)
 class RegexError(Spanned):
@@ -67,12 +122,18 @@ class RegexError(Spanned):
     """
     message: str
 
+@dataclass(frozen=True)
+class Comment(Spanned): ...
+
 type Regex = (
     RegexLiteral
+    | ZeroWidthRegexLiteral
     | AltEnd
-    | RepeatEnd
     | GroupStart
+    | Conditional
     | GroupEnd
+    | RepeatStart
+    | RepeatEnd
     | RegexError
+    | Comment
 )
-
